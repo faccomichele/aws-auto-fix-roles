8e1787668305e0ee8c5d42019976c1c13fb9e686
Michele Facco <faccomichele@gmail.com>
2026-04-25 18:25:38 +0700

    fix

--- definitions.tf
+++ definitions.tf
@@ -1,12 +1,11 @@
 locals {
   environment           = split("_", terraform.workspace)[0]
   aws_region            = split("_", terraform.workspace)[1]
+  organization          = var.tags["Organization"] != null ? var.tags["Organization"] : "unknown"
   project_name          = var.tags["Project"] != null ? var.tags["Project"] : "unknown"
   log_retention_in_days = local.environment == "prod" ? 30 : 7
-  github_token_ssm_path = "/${local.project_name}/${local.environment}/github-token"
+  github_token_ssm_path = "/${local.organization}/${local.project_name}/${local.environment}/github-token"
   python_runtime        = "python3.13"
 
-  tags = merge (var.tags, {
-    Project = local.project_name
-  })
+  tags = var.tags # No custom tags for now, but this allows to easily add them later on.
 }
